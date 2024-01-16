f6b41f24d2480f349ff7e42f4401cbe54747fd59
JorgEdu1 <jorge.eduardo@alu.ufc.br>
2024-01-16 10:37:27 -0300

    seguindo meus estudos com react

---
+++ 2_fundamentos/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Welcome from './components/Welcome'
+import BomDia from './components/BomDia'
+
+function App() {
+
+  return (
+    <>
+      <BomDia nome="João" />
+    </>
+  )
+}
+
+export default App
